00b43c3657c347854104d7594843f2aaf650a467
Karina Moura <57039290+kfmr@users.noreply.github.com>
2022-02-27 11:33:16 -0300

    fix index of api,models

--- api/index.js
+++ api/index.js
@@ -1,16 +1,14 @@
 const express = require('express')
 const app = express()
-
+const routes = require('../api/Routes/index')
 app.use(express.urlencoded({
     extended: true
 }));
 
 app.use(express.json());
+// instanciar o routes passando o app como parametro para a aplicação rodar
 
-
-app.get('/', (req, res) => {
-    res.status(200).json("rota")
-})
+routes(app)
 
 app.listen(3000, () => {
     console.log("Api executando")
